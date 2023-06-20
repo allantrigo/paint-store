0dd6d825e4ffe6841a7c097d2342ff7015dc0416
Allan Trigo <allantrigo@gec.inatel.br>
2023-06-20 11:06:03 -0300

    feat: update imports

--- src/app/register/register.module.ts
+++
@@ -1,9 +0,0 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { RegisterComponent } from './shared/register.component';
-
-@NgModule({
-  imports: [CommonModule],
-  declarations: [RegisterComponent],
-})
-export class RegisterModule {}
